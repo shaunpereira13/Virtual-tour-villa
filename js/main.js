f7b1b841d4786acdb77c588102342da3cd796bb0
shaun pereira <119792837+shaunpereira13@users.noreply.github.com>
2024-01-31 14:59:43 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let pano1= new PANOLENS.ImagePanorama( 'images/new/1.jpg' );
+let pano1= new PANOLENS.ImagePanorama( 'ddd' );
 let pano2 = new PANOLENS.ImagePanorama('images/new/2.jpg');
 let pano3 = new PANOLENS.ImagePanorama('images/new/3.jpg');
 let pano4 = new PANOLENS.ImagePanorama('images/new/4.jpg');
